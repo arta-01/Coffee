d7c6764266f1ceb2e64c90720f5160b5c2b9e942
elliot sad <bamdadmahdi05@gmail.com>
2023-02-14 23:55:30 +0100

    menu start

--- tour-management/frontend/src/components/Header/Header.jsx
+++ tour-management/frontend/src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ import { NavLink, Link } from "react-router-dom";
 
 // we do not have a logo for shop {mehdi :D}
 import logo from '../../assets/images/logo.png';
+import './header.css';
 
 const nav__links = [
   {
@@ -33,6 +34,31 @@ const Header = () => {
          {/* ========= logo end========= */}
 
           
+            {/* ========= menu start ========= */}
+            <div className="navigation">
+              <ul className="menu d-flex align-item-center gap-5">
+                {nav__links.map((item, index) => (
+                  <li className="nav__item">
+                    <NavLink to={item.path}>{item.display}</NavLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            {/* ========= menu end========= */}
+
+            <div className="nav__right d-flex align-item-center gap-4">
+              <div className="nav__btns d-flex align-item-center gap-4">
+                <button className="btn secondary__btn ">
+                  <link to="/login">Login</link>
+                </button>
+                <button className="btn priamry__btn ">
+                  <link to="/register">Register</link>
+                </button>
+              </div>
+              <span className="mobile__menu">
+                <i class="ri-menu-line"></i>
+              </span>
+            </div>
         </div>
       </Row>
     </Container>
